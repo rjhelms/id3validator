df1bbe983e9469daa5d95a278c6c216454b5602e
rjhelms <robhailman@gmail.com>
2020-12-04 16:01:51 -0500

    Add genre validation

--- id3validator.py
+++ id3validator.py
@@ -18,6 +18,7 @@ class ValidationMessages(Enum):
     MISSING_CATEGORY = "Missing category"
     INVALID_CATEGORY = "Invalid category"
     INVALID_GENRE = "Invalid item in genre"
+    CATEGORY_WRONG_POSITION = "Category in wrong position"
 
 
 ALL_CATEGORIES = (
@@ -43,6 +44,8 @@ ALL_CATEGORIES = (
     53,
 )
 
+ALL_GENRE_ITEMS = ("cancon", "local")
+
 
 @dataclass
 class TrackType:
@@ -54,6 +57,8 @@ class TrackType:
         Name of the track type
     valid_categories: optional
         Valid CRTC content categories for the track type, defaults to all valid CRTC categories.
+    valid_genre_items: optional
+        Other entries that are valid to appear in the "genre" tag. Defaulst to ALL_GENRE_ITEMS
     artist_mandatory: optional
         Whether the artist field is mandatory for this track type, defaults to False
     album_mandatory: optional
@@ -62,6 +67,7 @@ class TrackType:
 
     name: str
     valid_categories: Tuple[int] = ALL_CATEGORIES
+    valid_genre_items: Tuple[str] = ALL_GENRE_ITEMS
     artist_mandatory: bool = False
     album_mandatory: bool = False
 
@@ -91,6 +97,55 @@ class Track:
         with open(filename, "rb") as file_obj:
             self.metadata = EasyID3(file_obj)
 
+    def __validate_genre(self) -> bool:
+        genre_valid = True
+        if "genre" in self.metadata:
+            genres = []
+            for i in self.metadata["genre"]:
+                j = i.split(", ")
+                genres.extend(j)
+
+            # verify cat is present
+            contains_category = False
+            category_index = None
+            category = 0
+            for i, item in enumerate(genres):
+                if item[0:3] == "cat":
+                    contains_category = True
+                    category_index = i
+                    category = int(item[3:])
+                    break
+
+            if not contains_category:
+                self.__errors.append(ValidationMessages.MISSING_CATEGORY.value)
+                genre_valid = False
+
+            if contains_category:
+                # verify cat is in first position
+                if category_index is not 0:
+                    self.__warnings.append(
+                        ValidationMessages.CATEGORY_WRONG_POSITION.value
+                    )
+
+                # verify cat is valid
+                if category not in self.type.valid_categories:
+                    self.__errors.append(ValidationMessages.INVALID_CATEGORY.value)
+
+            # verify other genre items are acceptable
+            for i, item in enumerate(genres):
+                if i is not category_index:
+                    if item not in self.type.valid_genre_items:
+                        self.__errors.append(
+                            f"{ValidationMessages.INVALID_GENRE.value}: {item}"
+                        )
+                        genre_valid = False
+
+        else:
+            self.__errors.append(ValidationMessages.MISSING_CATEGORY.value)
+            genre_valid = False
+
+        return genre_valid
+
     def validate(self) -> bool:
         """Validates the metadata for the audio track.
 
@@ -105,31 +160,27 @@ class Track:
         valid_check = True
 
         if "title" not in self.metadata:
-            self.__errors.append(ValidationMessages.MISSING_TITLE)
+            self.__errors.append(ValidationMessages.MISSING_TITLE.value)
             valid_check = False
 
         if "album" not in self.metadata:
             if self.type.album_mandatory:
-                self.__errors.append(ValidationMessages.MISSING_ALBUM)
+                self.__errors.append(ValidationMessages.MISSING_ALBUM.value)
                 valid_check = False
             else:
-                self.__warnings.append(ValidationMessages.MISSING_ALBUM)
+                self.__warnings.append(ValidationMessages.MISSING_ALBUM.value)
 
         if "artist" not in self.metadata:
             if self.type.artist_mandatory:
-                self.__errors.append(ValidationMessages.MISSING_ARTIST)
+                self.__errors.append(ValidationMessages.MISSING_ARTIST.value)
                 valid_check = False
             else:
-                self.__warnings.append(ValidationMessages.MISSING_ARTIST)
+                self.__warnings.append(ValidationMessages.MISSING_ARTIST.value)
 
         if "date" not in self.metadata:
-            self.__warnings.append(ValidationMessages.MISSING_YEAR)
+            self.__warnings.append(ValidationMessages.MISSING_YEAR.value)
 
-        if "genre" in self.metadata:
-            # TODO: implement full genre validation
-            pass
-        else:
-            self.__errors.append(ValidationMessages.MISSING_CATEGORY)
+        if not self.__validate_genre():
             valid_check = False
 
         self.__valid = valid_check
@@ -169,11 +220,12 @@ class Track:
 
 if __name__ == "__main__":
 
-    input_file = input("File?")
-    input_file = input_file.strip("\"'")
+    while True:
+        input_file = input("File?")
+        input_file = input_file.strip("\"'")
 
-    track = Track(input_file)
+        track = Track(input_file)
 
-    print("Errors: ", track.errors)
-    print("Warnings: ", track.warnings)
-    print("Valid: ", track.valid)
+        print("Errors: ", track.errors)
+        print("Warnings: ", track.warnings)
+        print("Valid: ", track.valid)
