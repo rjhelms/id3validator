eaf201158d41acf95f1a15517c40176e63cfe8a1
rjhelms <robhailman@gmail.com>
2020-12-04 13:09:20 -0500

    Begin implementing track validation
    Genre tag not yet handled, except for it's verifying presence.

--- id3validator.py
+++ id3validator.py
@@ -1,9 +1,23 @@
 """Audio file metadata validator for Trent Radio's Libretime implementation."""
 
 
-from typing import Tuple
+from enum import Enum
+from typing import List, Tuple
 
 from mutagen.easyid3 import EasyID3
+
+
+class ValidationMessages(Enum):
+    """Enumerator to hold validation messages."""
+
+    MISSING_TITLE = "Missing title"
+    MISSING_ARTIST = "Missing artist"
+    MISSING_ALBUM = "Missing album"
+    MISSING_YEAR = "Missing year"
+    MISSING_CATEGORY = "Missing category"
+    INVALID_CATEGORY = "Invalid category"
+    INVALID_GENRE = "Invalid item in genre"
+
 
 ALL_CATEGORIES = (
     11,
@@ -75,17 +89,97 @@ class Track:
         """
         self.filename = filename
         self.type = tracktype
+        self.__errors = []
+        self.__warnings = []
+        self.__valid = False
+        self.__validated = False
 
-        # TODO: handle file path properly!
+        with open(filename, "rb") as file_obj:
+            self.metadata = EasyID3(file_obj)
 
-        file = open(filename, "rb")
-        self.metadata = EasyID3(file)
+    def validate(self) -> bool:
+        """Validates the metadate for the audio track.
+
+        Any errors and warnings generated validation can be retrieved from their respective
+        properties.
+
+        Returns
+        -------
+        bool
+            True if no validation errors were encountered, false otherwise
+        """
+        valid_check = True
+
+        if "title" not in self.metadata:
+            self.__errors.append(ValidationMessages.MISSING_TITLE)
+            valid_check = False
+
+        if "album" not in self.metadata:
+            if self.type.album_mandatory:
+                self.__errors.append(ValidationMessages.MISSING_ALBUM)
+                valid_check = False
+            else:
+                self.__warnings.append(ValidationMessages.MISSING_ALBUM)
+
+        if "artist" not in self.metadata:
+            if self.type.artist_mandatory:
+                self.__errors.append(ValidationMessages.MISSING_ARTIST)
+                valid_check = False
+            else:
+                self.__warnings.append(ValidationMessages.MISSING_ARTIST)
+
+        if "date" not in self.metadata:
+            self.__warnings.append(ValidationMessages.MISSING_YEAR)
+
+        if "genre" in self.metadata:
+            # TODO: implement full genre validation
+            pass
+        else:
+            self.__errors.append(ValidationMessages.MISSING_CATEGORY)
+            valid_check = False
+
+        self.__valid = valid_check
+        self.__validated = True
+        return self.__valid
+
+    @property
+    def errors(self) -> List[str]:
+        """
+        Returns the list of validation errors. If validation has not yet been performed, runs
+        validate() before returning. Read-only.
+        """
+        if not self.__validated:
+            self.validate()
+        return self.__errors
+
+    @property
+    def warnings(self) -> List[str]:
+        """
+        Returns the list of validation warnings. If validation has not yet been performed, runs
+        validate() before returning. Read-only.
+        """
+        if not self.__validated:
+            self.validate()
+        return self.__warnings
+
+    @property
+    def valid(self) -> bool:
+        """
+        Returns the stored validation result for the audio track. If validation has not yet been
+        performed, runs validate() before returning. Read-only.
+        """
+        if not self.__validated:
+            self.validate()
+        return self.__valid
 
 
 if __name__ == "__main__":
 
     input_file = input("File?")
-    input_file = input_file.strip('"')
+    input_file = input_file.strip("\"'")
 
     track = Track(input_file)
-    print(track.metadata)
+
+    print("Errors: ", track.errors)
+    print("Warnings: ", track.warnings)
+    print("Valid: ", track.valid)
